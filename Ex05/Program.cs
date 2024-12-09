b5b26b69da53fda0e8f8d96045d585f8570f1250
denisKharenko <denis2450@walla.co.il>
2024-12-09 14:57:58 +0200

    uploaded project code

---
+++ Ex05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace B23_Ex05_SharonOlshanetsky_DenisKharenko
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GameSettingsForm());
+        }
+    }
+}
